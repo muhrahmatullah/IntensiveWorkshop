b546482894ba9044122498b2b887997e069ec497
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-03-31 12:02:27 +0800

    delete unecessary stuff

--- app/src/main/java/com/rahmat/app/intensiveworkshop/MainActivity.java
+++ app/src/main/java/com/rahmat/app/intensiveworkshop/MainActivity.java
@@ -9,28 +9,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCount;
-    TextView txtCount;
-    int angka;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCount = findViewById(R.id.btnCount);
-        txtCount = findViewById(R.id.txtCount);
-
-        btnCount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                angka++;
-                txtCount.setText(String.valueOf(angka));
-            }
-        });
-    }
-
-    public void btnToast(View view) {
-        //Toast.makeText(this, "Halo beb, nomor kamu adalah "+angka, Toast.LENGTH_LONG).show();
     }
 }
